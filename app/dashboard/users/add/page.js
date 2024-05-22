6633340fba6433c48a11ee0030dfbee52e404aa6
Abhay Yadav <yadavabhay966@gmail.com>
2024-05-22 12:34:30 +0530

    Add new user Done!

--- app/dashboard/users/add/page.js
+++ app/dashboard/users/add/page.js
@@ -1,9 +1,76 @@
-import React from 'react'
+'use client'
+import { useForm } from "react-hook-form";
 
 const addUserPage = () => {
+
+    const {
+        register,
+        handleSubmit,
+        formState:{errors}
+    } = useForm();
+
   return (
-    <div>
-        abhi
+    <div className="border border-red-50 px-5 overflow-auto min-h-full w-full" >
+        <form className="" >
+            {/* username */}
+            <input 
+                type="text"
+                placeholder="Enter your name"
+                {...register("name", {required:true})}
+            />
+
+            {/* email */}
+            <input 
+                type="email"
+                placeholder="Enter your Email"
+                {...register("email", {required:true})}
+            />
+
+            {/* Password */}
+            <input 
+                type="password" 
+                placeholder="Enter your Password"
+                {...register("password", {required:true})}
+            />
+
+            {/* Phone No. */}
+            <input 
+                type="text" 
+                placeholder="Enter your Phone No."
+                {...register("phone", {required:true})}
+            />
+
+            {/* Admin DropDown */}
+            <select
+                {...register("isAdmin",{required:true})}
+            >
+                <option value="true">isAdmin</option>
+                <option value="true">Yes</option>
+                <option value="false">No</option>
+            </select>
+
+            {/* Active Dropdown */}
+            <select
+                {...register("active", {required:true})}
+            >
+                <option value="true">Yes</option>
+                <option value="false">No</option>
+            </select>
+
+            {/* address */}
+            <input 
+                type="text"
+                placeholder="Enter your Address"
+                {...register("address", {required:true})}
+            />
+
+            {/* button */}
+            <button 
+                className=""
+            >
+                Submit
+            </button>
+        </form>
     </div>
   )
 }
